2a5c2884096ba798d787531c45418b7a2adf8c27
xuatz <xzlow10@gmail.com>
2017-04-20 02:53:40 +0800

    fix render bug
    it was simply because of a missing `this` keyword in `SessionCheckModule.js`

--- actions/userActions.js
+++ actions/userActions.js
@@ -1,38 +1,38 @@
-import Router from 'next/router';
-import axios from 'axios';
+import Router from 'next/router'
+import axios from 'axios'
 
 let API = axios.create({
-    baseURL: 'http://' + process.env.API_URL,
-    withCredentials: true,
-    timeout: 5000,
-});
+	baseURL: 'http://' + process.env.API_URL,
+	withCredentials: true,
+	timeout: 5000,
+})
 
 export const signup = form => {
-    return dispatch => {
-        return API.post('/signup', {
-            ...form,
-        })
-            .then(res => {
-                console.log(res)
-                if (res.data && res.data.error) {
-                    return {
-                        error: res.data.error,
-                    };
-                }
+	return dispatch => {
+		return API.post('/signup', {
+			...form,
+		})
+			.then(res => {
+				console.log(res)
+				if (res.data && res.data.error) {
+					return {
+						error: res.data.error,
+					}
+				}
 
-                if (res.status == 200) {
-                    // dispatch({
-                    //     type: 'USER_LOGGED_IN',
-                    //     user: res.data.user
-                    // })
-                }
+				if (res.status == 200) {
+					dispatch({
+						type: 'USER_LOGGED_IN',
+						user: res.data.user,
+					})
+				}
 
-                return {
-                    status: res.status,
-                };
-            })
-            .catch(err => {
-                throw err;
-            });
-    };
-};
+				return {
+					status: res.status,
+				}
+			})
+			.catch(err => {
+				throw err
+			})
+	}
+}
