47fa019d7e36a13974e78d80d46c058e8ce304ed
xuatz <xzlow10@gmail.com>
2017-05-05 01:37:35 +0800

    added simple logout href

--- components/common/Header.js
+++ components/common/Header.js
@@ -1,18 +1,34 @@
 import Link from 'next/link';
+import axios from 'axios';
 
 const linkStyle = {
-    marginRight: 15,
+	marginRight: 15
 };
 
 const Header = () => (
-    <div>
-        <Link href="/">
-            <a style={linkStyle}>Home</a>
-        </Link>
-        <Link href="/about">
-            <a style={linkStyle}>About</a>
-        </Link>
-    </div>
+	<div>
+		<Link href="/">
+			<a style={linkStyle}>Home</a>
+		</Link>
+		<Link href="/about">
+			<a style={linkStyle}>About</a>
+		</Link>
+		<a
+			href="#"
+			onClick={() => {
+				let API = axios.create({
+					baseURL: 'http://' + process.env.API_URL,
+					withCredentials: true,
+					timeout: 5000
+				});
+
+				API.get('/logout').then(res => {
+					console.log(res);
+				});
+			}}>
+			shitty logout
+		</a>
+	</div>
 );
 
 export default Header;
