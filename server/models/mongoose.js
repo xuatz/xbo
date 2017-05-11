f29300f2637807043233107e6427ec219ac200a0
xuatz <xzlow10@gmail.com>
2017-05-11 18:44:09 +0800

    code refactoring for mongoose config

--- server/models/mongoose.js
+++ server/models/mongoose.js
@@ -1,47 +1,42 @@
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
-// let dbConnectionString = 'mongodb://';
-// if (config.db.host) {
-//  dbConnectionString += config.db.host;
-// }
-// if (config.db.port) {
-//  dbConnectionString += ":" + config.db.port;
-// }
-// dbConnectionString += "/" + config.db.name;
+let dbConnectionString = 'mongodb://';
+dbConnectionString += process.env.DB_HOST || 'localhost';
+if (process.env.DB_PORT) {
+	dbConnectionString += ':' + process.env.DB_PORT;
+}
+dbConnectionString += '/' + process.env.DB_NAME || 'xbo_development';
 
 let options = Object.assign(
-    {
-        // db: 'xpo',
-        server: {
-            poolSize: 5,
-            auto_reconnect: true,
-            reconnectTries: 3,
-            socketOptions: {
-                keepAlive: 100,
-                connectTimeoutMS: 5000,
-                socketTimeoutMS: 30000,
-            },
-        },
-    },
-    {
-        user: process.env.DB_USER,
-        pass: process.env.DB_PASSWORD,
-    }
+	{
+		server: {
+			poolSize: 5,
+			auto_reconnect: true,
+			reconnectTries: 3,
+			socketOptions: {
+				keepAlive: 100,
+				connectTimeoutMS: 5000,
+				socketTimeoutMS: 30000
+			}
+		}
+	},
+	{
+		user: process.env.DB_USER,
+		pass: process.env.DB_PASSWORD
+	}
 );
 
-let host = process.env.DB_HOST || 'localhost';
-
-mongoose.connect('mongodb://' + host + '/xpo', options);
+mongoose.connect(dbConnectionString, options);
 
 mongoose.connection.on('error', function(error) {
-    console.log('mongoose err', error);
+	console.log('mongoose err', error);
 });
 mongoose.connection.on('connected', function() {
-    console.log('Connection established to MongoDB');
+	console.log('Connection established to MongoDB');
 });
 mongoose.connection.on('reconnected', function() {
-    console.log('Reconnected to MongoDB');
+	console.log('Reconnected to MongoDB');
 });
 
 module.exports = mongoose;
