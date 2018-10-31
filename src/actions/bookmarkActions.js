081dbb9e2703a4c315beb2c84e69fbf5055d3fad
xuatz <xzlow10@gmail.com>
2018-10-31 22:52:34 +0900

    relocate some sublist logic from component to reducer

--- src/actions/bookmarkActions.js
+++ src/actions/bookmarkActions.js
@@ -1,60 +1,56 @@
 import axios from "axios";
 
 let API = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || "http://localhost:9000",
-    withCredentials: true,
-    timeout: 5000
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:9000",
+  withCredentials: true,
+  timeout: 5000
 });
 
 export const deleteBookmark = id => {
-    return dispatch => {
-        dispatch({
-            type: "BOOKMARKS_REMOVE_BY_ID",
-            id
-        });
-
-        return API.delete("/bookmarks/" + id).then(res => {
-            // if (res.status === 200) {
-            // }
-        });
-    };
+  console.log("deleteBookmark", id);
+  return dispatch => {
+    dispatch({
+      type: "BOOKMARKS_REMOVE_BY_ID",
+      id
+    });
+    return API.delete("/bookmarks/" + id).then(res => {
+      // if (res.status === 200) {
+      // }
+    });
+  };
 };
 
 export const getBookmarksUncategorised = () => {
-    return dispatch => {
-        return API.get("/bookmarks?type=magic").then(res => {
-            if (res.status === 200) {
-                dispatch({
-                    type: "BOOKMARKS_UNCATEGORISED_REPLACE",
-                    uncategorised: res.data || []
-                });
-            }
+  return dispatch => {
+    return API.get("/bookmarks?type=magic").then(res => {
+      if (res.status === 200) {
+        dispatch({
+          type: "BOOKMARKS_UNCATEGORISED_REPLACE",
+          uncategorised: res.data || []
         });
-    };
+      }
+    });
+  };
 };
 
 export const fetchBookmarks = () => {
-    return dispatch => {
-        //'api.xbo.xuatz.com'
-        return API.get("/bookmarks")
-            .then(res => {
-                dispatch({
-                    type: "BOOKMARKS_REPLACE",
-                    bookmarks: res.data || []
-                });
-                return API.get("/bookmarks/fetch", {
-                    timeout: 0
-                });
-            })
-            .then(res => {
-                dispatch({
-                    type: "BOOKMARKS_REPLACE",
-                    bookmarks: res.data || []
-                });
-                dispatch({ type: "BOOKMARKS_GROUP_BY_DOMAIN" });
-                dispatch({
-                    type: "BOOKMARKS_GROUP_BY_HASHTAG"
-                });
-            });
-    };
+  return dispatch => {
+    //'api.xbo.xuatz.com'
+    return API.get("/bookmarks")
+      .then(res => {
+        dispatch({
+          type: "BOOKMARKS_REPLACE",
+          bookmarks: res.data || []
+        });
+        return API.get("/bookmarks/fetch", {
+          timeout: 0
+        });
+      })
+      .then(res => {
+        dispatch({
+          type: "BOOKMARKS_REPLACE",
+          bookmarks: res.data || []
+        });
+      });
+  };
 };
--- src/reducers/bookmarksReducer.js
+++ src/reducers/bookmarksReducer.js
@@ -1,94 +1,133 @@
 import _ from "lodash";
 
 const groupByDomain = bookmarks => {
-    let tmp = _.groupBy(bookmarks, bk => bk.stats && bk.stats.domain);
+  let tmp = _.groupBy(bookmarks, bk => bk.stats && bk.stats.domain);
 
-    let domains = [];
-    _.forIn(tmp, (value, key) => {
-        if (key !== "undefined") {
-            domains.push({
-                domain: key,
-                bookmarks: value.map(bk => {
-                    return bk._id;
-                })
-            });
-        }
-    });
+  let domains = [];
+  _.forIn(tmp, (value, key) => {
+    if (key !== "undefined") {
+      domains.push({
+        domain: key,
+        bookmarks: value.map(bk => {
+          return bk._id;
+        })
+      });
+    }
+  });
 
-    return domains;
+  return domains;
 };
 
 const groupByHashtag = bookmarks => {
-    const hashtags = ["guide", "todo"];
+  const hashtags = ["guide", "todo"];
 
-    let tmp = {};
+  let tmp = {};
 
-    bookmarks.map((bk, index) => {
-        let { body } = bk.data;
+  bookmarks.map((bk, index) => {
+    let { body } = bk.data;
 
-        if (body) {
-            hashtags.map(hashtag => {
-                if (body.toLowerCase().indexOf("#" + hashtag) !== -1) {
-                    if (!tmp[hashtag]) {
-                        tmp[hashtag] = [];
-                    }
-                    tmp[hashtag].push(bk._id);
-                }
-                return true;
-            });
+    if (body) {
+      hashtags.map(hashtag => {
+        if (body.toLowerCase().indexOf("#" + hashtag) !== -1) {
+          if (!tmp[hashtag]) {
+            tmp[hashtag] = [];
+          }
+          tmp[hashtag].push(bk._id);
         }
-
         return true;
-    });
-    return tmp;
+      });
+    }
+
+    return true;
+  });
+  return tmp;
 };
 
 const initialState = {
-    bookmarks: [],
-    uncategorised: []
+  bookmarks: [],
+  uncategorised: []
 };
 
 export default (state = initialState, action) => {
-    switch (action.type) {
-        case "BOOKMARKS_REMOVE_BY_ID": {
-            return {
-                ...state,
-                uncategorised: state.uncategorised.filter(bk => {
-                    return bk._id !== action.id;
-                }),
-                bookmarks: state.bookmarks.filter(bk => {
-                    return bk._id !== action.id;
-                })
-            };
-        }
-        case "BOOKMARKS_UNCATEGORISED_REPLACE": {
-            return {
-                ...state,
-                uncategorised: action.uncategorised
-            };
-        }
-        case "BOOKMARKS_REPLACE":
-            return {
-                ...state,
-                bookmarks: action.bookmarks || state.bookmarks
-            };
-        case "BOOKMARKS_GROUP_BY_DOMAIN":
-            return {
-                ...state,
-                stats: {
-                    ...state.stats,
-                    groupByDomain: groupByDomain(state.bookmarks)
-                }
-            };
-        case "BOOKMARKS_GROUP_BY_HASHTAG":
-            return {
-                ...state,
-                stats: {
-                    ...state.stats,
-                    groupByHashtag: groupByHashtag(state.bookmarks)
-                }
-            };
-        default:
-            return state;
+  switch (action.type) {
+    case "BOOKMARKS_REMOVE_BY_ID": {
+      return {
+        ...state,
+        ...bookmarksReplace(
+          {
+            ...state,
+            bookmarks: state.bookmarks.filter(bk => {
+              return bk._id !== action.id;
+            })
+          },
+          action
+        ),
+        uncategorised: state.uncategorised.filter(bk => {
+          return bk._id !== action.id;
+        })
+      };
     }
+    case "BOOKMARKS_UNCATEGORISED_REPLACE": {
+      return {
+        ...state,
+        uncategorised: action.uncategorised
+      };
+    }
+    case "BOOKMARKS_REPLACE":
+      return bookmarksReplace(state, action);
+    case "BOOKMARKS_GROUP_BY_DOMAIN":
+      return {
+        ...state,
+        stats: {
+          ...state.stats,
+          groupByDomain: groupByDomain(state.bookmarks)
+        }
+      };
+    case "BOOKMARKS_GROUP_BY_HASHTAG":
+      return {
+        ...state,
+        stats: {
+          ...state.stats,
+          groupByHashtag: groupByHashtag(state.bookmarks)
+        }
+      };
+    default:
+      return state;
+  }
+};
+
+const bookmarksReplace = (state, action) => {
+  let all = action.bookmarks || state.bookmarks;
+
+  let sublists = {};
+  all = all
+    .sort((a, b) => {
+      return b.data.modified - a.data.modified;
+    })
+    .map(bk => {
+      if (!sublists[bk.data.type]) {
+        sublists[bk.data.type] = [];
+      }
+      sublists[bk.data.type].push(bk);
+      return bk;
+    });
+
+  if (sublists && sublists.file) {
+    sublists.gallery = [].concat(
+      sublists.file.filter(item => {
+        return item.data.image_url ? true : false;
+      })
+    );
+  }
+
+  return {
+    ...state,
+    bookmarks: all,
+    sublists: sublists,
+    stats: {
+      ...state.stats,
+      groupByHashtag: groupByHashtag(state.bookmarks),
+      groupByDomain: groupByDomain(state.bookmarks)
+    }
+  };
 };
